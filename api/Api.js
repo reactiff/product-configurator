6f5c3437e977959cc8ae882dde3665e1c7830751
Rick Ellis <rickursive@gmail.com>
2020-03-10 01:55:45 -0500

    Add files via upload

---
+++ api/Api.js
@@ -0,0 +1,11 @@
+import LocalStorageDictionary from "./LocalStorageDictionary";
+
+
+export default class Api extends LocalStorageDictionary {
+
+    constructor(path) {
+        super('assemblies');
+    }
+    
+}
+
